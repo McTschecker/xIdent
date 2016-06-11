1224cacfb64a17faf7734a12b15402624dcc1ca2
McTschecker <Fabian@wirsindblank.de>
2016-06-11 12:34:45 +0200

    Added Login php

---
+++ passcode.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html><head>
+<title>HTML KickStart Elements</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<meta name="description" content="" />
+<meta name="copyright" content="" />
+<link rel="stylesheet" type="text/css" href="css/kickstart.css" media="all" />                  <!-- KICKSTART -->
+<link rel="stylesheet" type="text/css" href="style.css" media="all" />                          <!-- CUSTOM STYLES -->
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script type="text/javascript" src="js/kickstart.js"></script>                                  <!-- KICKSTART -->
+</head><body>
+<div class="grid">
+	<div class="col_12" style="margin-top:100px;">
+		<h1 class="center">
+		<label for="text1">PASSCODE</label>
+<input id="text1" type="text" />
+	</div>
+
+
+
+<!-- ===================================== START FOOTER =====================================
+<div class="clear"></div>
+<div id="footer">
+&copy; Copyright 2011–2012 All Rights Reserved. This website was built with <a href="http://www.99lime.com">HTML KickStart</a>
+</div> -->
+
+</body></html>
