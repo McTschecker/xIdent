0818270615cfd3891b6ce27c01761c662a63c711
Moritz Beck <moritz@mvbeck.de>
2016-06-11 17:20:59 +0200

    präsi start

--- install/install.sh
+++ install/install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 #für Raspi unter Rasbian Linux
+sudo -i
 apt-get update && apt-get upgrade
+apt-get install dialog
 apt-get install apache2 php5-mysql libapache2-mod-php5 mysql-server
-apt-get install nano
+apt-get install vsftpd 
+
+exit
